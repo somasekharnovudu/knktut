986e843853f1ed103c3176523a4fe6c9a55f8f1c
novudu <novudu.somasekhar702@gmail.com>
2018-09-02 21:50:56 +0530

    Es6 Solution added for sum of digits in a number till single digit

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function alertFunction(){
     altText2.hidden=false;
     alert(altText1);
 }
+const valadder=(event)=>{
+    let x=event.target.value;
+    while(x.length>1){
+        x=digitAdder(x);
+    }
+    document.querySelector('#result').value=x;
+}
+const digitAdder=(value)=>{
+    const val=value.split('');
+    const res= val.reduce((a,b)=>{
+        return +a+(+b);
+    });
+    return ''+res;
+}
